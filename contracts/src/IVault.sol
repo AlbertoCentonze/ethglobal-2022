df93ac699beab7039f866de46f1ad77491a67393
Alberto Centonze <albicento.ac@gmail.com>
2022-09-08 14:36:27 +0200

    Cleaned boilerplate files

---
+++ contracts/src/IVault.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+interface IVault {
+	function deposit(uint256 amount) external; //TODO nonReentrant
+	function withdraw(uint256 amount) external; //TODO nonReentrant
+	function claim() external;
+}
