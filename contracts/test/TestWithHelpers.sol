036cdb53ed47f4b669f6690bf8c94ac64aaf849f
Alberto Centonze <albicento.ac@gmail.com>
2022-09-12 15:32:11 +0200

    Removed helper that wasn't really helping

--- contracts/test/TestWithHelpers.sol
+++ contracts/test/TestWithHelpers.sol
@@ -5,17 +5,8 @@ import "@forge-std/Test.sol";
 import "@openzeppelin/utils/Counters.sol";
 
 /**
-Helper class with some functions to simplify tests
+Helper class with some functions and functions to simplify tests
  */
 abstract contract TestWithHelpers is Test {
     address internal DEV_FUND = address(657489);
-
-    using Counters for Counters.Counter;
-    Counters.Counter counter;
-
-    function getRandomAddress() public returns (address randomAddress) {
-        randomAddress = address(uint160(counter.current()));
-        counter.increment();
-        vm.deal(randomAddress, 100000 ether);
-    }
 }
