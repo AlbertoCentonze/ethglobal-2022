c2a15f659c5cb51c264d673d4c647a4c5c77c6a9
Alberto Centonze <albicento.ac@gmail.com>
2022-09-14 15:27:19 +0200

    Updated tests

--- contracts/test/TestWithHelpers.sol
+++ contracts/test/TestWithHelpers.sol
@@ -5,7 +5,7 @@ import "@forge-std/Test.sol";
 import "@openzeppelin/utils/Counters.sol";
 
 /**
- * Helper class with some functions and functions to simplify tests
+ * Helper class with some functions and variables to simplify tests
  */
 abstract contract TestWithHelpers is Test {
     address internal DEV_FUND = address(657489);
