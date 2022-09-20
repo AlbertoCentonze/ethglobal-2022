af16e33527366624bceb677234de87a8c80a8429
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2022-09-20 15:49:40 +0200

    EnsRegistrar starting

--- contracts/src/EnsCrossChain.sol
+++ contracts/src/EnsCrossChain.sol
@@ -5,17 +5,22 @@ import "@solmate/auth/Owned.sol";
 
 contract EnsCrossChain is owned{
     address public ensRegistrar;
-    //the ensRegistrar's functions' selectors
+    //TODO: edit the functions arguments / the ensRegistrar's functions' selectors 
     bytes4 internal mintSelector = bytes4(keccak256("mintSubDomain(uint256)"));
     bytes4 internal burnSelector = bytes4(keccak256("burnSubDomain(uint256)"));
 
-    uint32 originDomain; //e.g. from Goerli (1735353714) TDO: trouver le originDomain pour Polygon
+    address targetContract;
+
+    uint32 originDomain; //TODO: trouver le originDomain pour Polygon / e.g. from Goerli (1735353714) 
+    uint32 destinationDomain; //TODO: trouver le destinationDomain pour Ethereum Mainnet / e.g. to Optimism-Goerli (1735356532) 
 
     IConnextHandler public immutable connext;
 
-    constructor(IConnextHandler _connext, uint32 _originDomain) {
+    constructor(IConnextHandler _connext, address _targetContract ,uint32 _originDomain, uint32 _destinationDomain) {
         connext = _connext;
         originDomain = _originDomain;
+        destinationDomain = _destinationDomain;
+        targetContract = _targetContract;
     }
 
     //setter for the ensRegistrar address
@@ -26,10 +31,9 @@ contract EnsCrossChain is owned{
     // This function will call the Registrar to mint the ENS-subdomain.
     function xMintSubDomain(
         address to, // the address of the target contract
-        uint32 destinationDomain, //TODO: have it in the constructor, to Optimism-Goerli (1735356532)
-        address subDomainRecepient, // the address receiving the subdomain
+        address subDomainRecepient, //TODO: maybe not needed / the address receiving the subdomain 
         uint32 nftId //the NFT ID
-        //TODO: add an argument of a name (packedString maybe)
+        //TODO: add an argument of a name (packedString maybe?)
     ) external payable {
         // the selector of this function is "mintSelector"
         //TODO: add the 3rd (name)
@@ -61,5 +65,36 @@ contract EnsCrossChain is owned{
     }
 
 
-    //TODO: do xBurnSubdomain:
+    // This function will call the Registrar to burn the ENS-subdomain.
+    function xMintSubDomain(
+        address to, // the address of the target contract
+        uint32 nftId //the NFT ID
+    ) external payable {
+        // the selector of this function is "burnSelector"
+        bytes memory callData = abi.encodeWithSelector(burnSelector, nftId);
+
+        CallParams memory callParams = CallParams({
+        to: ensRegistrar,
+        callData: callData,
+        originDomain: originDomain,
+        destinationDomain: destinationDomain,
+        agent: msg.sender, // address allowed to execute transaction on destination side in addition to relayers
+        recovery: msg.sender, // fallback address to send funds to if execution fails on destination side
+        forceSlow: true, // this must be true for authenticated calls
+        receiveLocal: false, // option to receive the local bridge-flavored asset instead of the adopted asset
+        callback: address(0), // zero address because we don't expect a callback
+        callbackFee: 0, // fee paid to relayers for the callback; no fees on testnet
+        relayerFee: 0, // fee paid to relayers for the forward call; no fees on testnet
+        destinationMinOut: 0 // not sending funds so minimum can be 0
+        });
+
+        XCallArgs memory xcallArgs = XCallArgs({
+        params: callParams,
+        transactingAsset: address(0), // 0 address is the native gas token
+        transactingAmount: 0, // not sending funds with this calldata-only xcall
+        originMinOut: 0 // not sending funds so minimum can be 0
+        });
+
+        connext.xcall(xcallArgs);
+    }
 }
