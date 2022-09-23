412a925880190558452bc43b8ff6511ea70f0e12
Alberto Centonze <albicento.ac@gmail.com>
2022-09-23 17:13:02 +0200

    Uploading changes for office hours

--- contracts/test/MaticTest.sol
+++ contracts/test/MaticTest.sol
@@ -2,15 +2,24 @@
 pragma solidity ^0.8.13;
 
 import "@forge-std/Test.sol";
-import {ISuperfluid} from "@superfluid-finance/interfaces/superfluid/ISuperfluid.sol";
 
 import {IInstantDistributionAgreementV1} from
     "@superfluid-finance/interfaces/agreements/IInstantDistributionAgreementV1.sol";
 
+import {ISuperfluidToken} from "@superfluid-finance/interfaces/superfluid/ISuperfluidToken.sol";
+import {Test} from "@forge-std/Test.sol";
+import {
+    Superfluid,
+    ConstantFlowAgreementV1,
+    InstantDistributionAgreementV1,
+    SuperTokenFactory,
+    SuperfluidFrameworkDeployer
+} from "@superfluid-finance/utils/SuperfluidFrameworkDeployer.sol";
+import {ERC1820RegistryCompiled} from "@superfluid-finance/libs/ERC1820RegistryCompiled.sol";
+import {CFAv1Library} from "@superfluid-finance/apps/CFAv1Library.sol";
 import {IDAv1Library} from "@superfluid-finance/apps/IDAv1Library.sol";
 import {ISuperfluidToken} from "@superfluid-finance/interfaces/superfluid/ISuperfluidToken.sol";
-
-
+import {ISuperToken} from "@superfluid-finance/interfaces/superfluid/ISuperToken.sol";
 
 abstract contract MaticTest is Test {
     uint256 maticFork;
@@ -24,17 +33,25 @@ abstract contract MaticTest is Test {
 abstract contract MaticSuperfluidTest is MaticTest {
     address maticSuperfluidHost = 0x3E14dC1b13c488a8d5D310918780c983bD5982E7;
     address maticIdaAddress = 0xB0aABBA4B2783A72C52956CDEF62d438ecA2d7a1;
-		address polETHx = 0x27e1e4E6BC79D93032abef01025811B7E4727e85;
+    address polETHx = 0x27e1e4E6BC79D93032abef01025811B7E4727e85;
     // use the IDAv1Library for the InitData struct
+
     using IDAv1Library for IDAv1Library.InitData;
 
     // declare `_idaLib` of type InitData
     IDAv1Library.InitData internal idaLib;
-		ISuperfluid sf = ISuperfluid(maticSuperfluidHost);
+    Superfluid host = Superfluid(maticSuperfluidHost);
 
-    constructor(/*ISuperfluid host, IInstantDistributionAgreementV1 ida*/) {
-        // assign it the host and ida addresses
-				// (bool something,,,) = idaLib.getSubscription(ISuperfluidToken(0x0000000000000000000000000000000000000001), 0x0000000000000000000000000000000000000001, 50, 0x0000000000000000000000000000000000000001);
+    function getIdaLibrary() public {
+        idaLib = IDAv1Library.InitData(
+            host,
+            IInstantDistributionAgreementV1(
+                address(
+                    host.getAgreementClass(
+                        keccak256("org.superfluid-finance.agreements.InstantDistributionAgreement.v1")
+                    )
+                )
+            )
+        );
     }
-    // ...
 }
--- contracts/test/Shirtless.t.sol
+++ contracts/test/Shirtless.t.sol
@@ -5,17 +5,17 @@ import "./MaticTest.sol";
 import "./TestWithHelpers.sol";
 
 contract ShirtlessTest is MaticSuperfluidTest, TestWithHelpers {
-    IDAv1Library.InitData internal _idaLiba = IDAv1Library.InitData(maticIdaAddress);
 
-    ISuperfluidToken ETHx = ISuperfluidToken(polETHx);
+    ISuperToken ETHx = ISuperToken(polETHx);
+		uint32 INDEX_ID = 0;
 
     function setUp() public {
-				activateFork();
+        activateFork();
+				getIdaLibrary();
+				console.log(ETHx.getHost());
     }
 
     function testSomething() public {
-        //sf.ida.createIndex(ETHx, 0, "");
-        //sf.ida.updateSubscription(ETHx, 0, RANDOM, 10, "");
-        //sf.ida.listSubscriptions(ETHx, RANDOM);
+			idaLib.createIndex(ETHx, INDEX_ID);
     }
 }
