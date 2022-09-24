b0f30ecadbd16ee8ce33adbc97a307eab5ab9766
Alberto Centonze <albicento.ac@gmail.com>
2022-09-24 20:15:56 +0200

    Fixed solidity compiler being broken

--- contracts/test/MaticTest.sol
+++ contracts/test/MaticTest.sol
@@ -31,27 +31,9 @@ abstract contract MaticTest is Test {
 }
 
 abstract contract MaticSuperfluidTest is MaticTest {
-    address maticSuperfluidHost = 0x3E14dC1b13c488a8d5D310918780c983bD5982E7;
-    address maticIdaAddress = 0xB0aABBA4B2783A72C52956CDEF62d438ecA2d7a1;
-    address polETHx = 0x27e1e4E6BC79D93032abef01025811B7E4727e85;
     // use the IDAv1Library for the InitData struct
 
-    using IDAv1Library for IDAv1Library.InitData;
-
     // declare `_idaLib` of type InitData
-    IDAv1Library.InitData internal idaLib;
-    Superfluid host = Superfluid(maticSuperfluidHost);
 
-    function getIdaLibrary() public {
-        idaLib = IDAv1Library.InitData(
-            host,
-            IInstantDistributionAgreementV1(
-                address(
-                    host.getAgreementClass(
-                        keccak256("org.superfluid-finance.agreements.InstantDistributionAgreement.v1")
-                    )
-                )
-            )
-        );
-    }
+    function getIdaLibrary() public {}
 }
