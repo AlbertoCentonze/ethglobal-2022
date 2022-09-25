0f7ce5057aab2ed0913596b5495c14577fc3baac
Alberto Centonze <albicento.ac@gmail.com>
2022-09-25 18:48:35 +0200

    Finished rewarder implementation

--- contracts/src/Rewarder.sol
+++ contracts/src/Rewarder.sol
@@ -21,10 +21,20 @@ contract Rewarder is Owned {
     address public rewardSuperToken;
     uint32 indexId;
 
-    constructor(address _collection, address _rewardSuperToken, uint32 _indexId) Owned(_collection) {
+    constructor(address _collection, address _rewardSuperToken, uint32 _indexId, address _host) Owned(_collection) {
+        // Superfluid setup
+        host = Superfluid(_host);
+        bytes32 classKeccak = keccak256("org.superfluid-finance.agreements.InstantDistributionAgreement.v1");
+        address idaAddress = address(host.getAgreementClass(classKeccak));
+        ida = IDAv1Library.InitData(host, InstantDistributionAgreementV1(idaAddress));
+
+        // Contract setup
         collection = _collection;
         rewardSuperToken = _rewardSuperToken;
         indexId = _indexId;
+
+        // Creates a new ida index
+        ida.createIndex(ISuperToken(_rewardSuperToken), _indexId);
     }
 
     function setIdaId(uint32 id) public onlyOwner {
@@ -38,5 +48,11 @@ contract Rewarder is Owned {
     function updateUnitsFromBalanceOf(address owner) public onlyOwner {
         uint128 ownerBalance = uint128(ERC721(collection).balanceOf(owner));
         ida.updateSubscriptionUnits(ISuperToken(rewardSuperToken), indexId, owner, ownerBalance);
+    }
+
+    // Permissionless, anyone can send the rewards at any time if he's willing to pay for gas fees
+    function sendRewards() public {
+        uint256 distributionAmount = ISuperToken(rewardSuperToken).balanceOf(address(this));
+        ida.distribute(ISuperToken(rewardSuperToken), indexId, distributionAmount);
     }
 }
--- contracts/src/Shirtless.sol
+++ contracts/src/Shirtless.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "./Rewarder.sol";
+
 import "@solmate/tokens/ERC721.sol";
 import "@solmate/auth/Owned.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -9,48 +11,22 @@ import {Superfluid, InstantDistributionAgreementV1} from "@superfluid-finance/ut
 import {IDAv1Library} from "@superfluid-finance/apps/IDAv1Library.sol";
 import {ISuperToken} from "@superfluid-finance/interfaces/superfluid/ISuperToken.sol";
 
-// Shirtless is just a placeholder name
-/**
- * ERC721
- */
 contract Shirtless is ERC721, Owned {
     using Counters for Counters.Counter;
-    using IDAv1Library for IDAv1Library.InitData;
-
-    IDAv1Library.InitData internal ida;
-    Superfluid host;
 
     Counters.Counter public circulatingSupply;
     Counters.Counter public mintId;
 
-    address rewardToken; //TODO Make setter ?
-    uint32 indexId; //TODO
+    address rewarder;
 
-    constructor(address superfluidHost, address _rewardToken, uint32 _indexId)
-        ERC721("Shirtless", "STL")
-        Owned(msg.sender)
-    {
-        host = Superfluid(superfluidHost);
-        ida = IDAv1Library.InitData(
-            host,
-            InstantDistributionAgreementV1(
-                address(
-                    host.getAgreementClass(
-                        keccak256("org.superfluid-finance.agreements.InstantDistributionAgreement.v1")
-                    )
-                )
-            )
-        );
-
-        ida.createIndex(ISuperToken(address(this)), 0);
-        rewardToken = _rewardToken;
-        indexId = _indexId;
+    constructor(address _rewarder) ERC721("Shirtless", "STL") Owned(msg.sender) {
+        rewarder = _rewarder;
     }
 
     function transferFrom(address from, address to, uint256 id) public override (ERC721) {
         ERC721.transferFrom(from, to, id);
-        // updateUnitsAccordingToBalance(from);
-        // updateUnitsAccordingToBalance(to);
+        Rewarder(rewarder).updateUnitsFromBalanceOf(from);
+        Rewarder(rewarder).updateUnitsFromBalanceOf(to);
     }
 
     function tokenURI(uint256 id) public pure override returns (string memory) {
@@ -61,12 +37,12 @@ contract Shirtless is ERC721, Owned {
         mintId.increment();
         circulatingSupply.increment();
         _safeMint(to, id, data);
-        // updateUnitsAccordingToBalance(to);
+        Rewarder(rewarder).updateUnitsFromBalanceOf(to);
     }
 
     function burn(uint256 id) public onlyOwner {
         circulatingSupply.decrement();
         _burn(id);
-        // updateUnitsAccordingToBalance(ownerOf(id));
+        Rewarder(rewarder).updateUnitsFromBalanceOf(ownerOf(id));
     }
 }
